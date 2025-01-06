f90629a872253dd0634e8041811457d9275f6942
soham408 <bambadesoham408@gmail.com>
2025-01-06 17:36:46 +0530

    first commit

---
+++ src/app/shot/layout.tsx
@@ -0,0 +1,16 @@
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+   
+
+        <>
+        <h2>inner layout item</h2>
+        {children}
+        </>
+  );
+}
