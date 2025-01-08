03bf1d51d025f958b296c3773c372792d6e43dd6
soham408 <bambadesoham408@gmail.com>
2025-01-08 13:25:45 +0530

    navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { main } from "framer-motion/client";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="text-4xl text-center">my clicked images</h1>
+    <main className="min-h-screen bg-black/[096] antialisaed bg-grid-white/[0.02]">
+    <h1 className="text-4xl text-center">Photography</h1>
+    </main>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,40 @@
-"use client";
+'use client';
+
 import React, { useState } from "react";
 import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
 import { cn } from "@/utils/cn";
 import Link from "next/link";
 
-
 function Navbar({ className }: { className?: string }) {
-  const [active, setActive] = useState<string | null>(null);
+    const [active, setActive] = useState<string | null>(null);
   return (
-    <div className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}>
-        <Link href={"#"}>
-        <MenuItem setActive={setActive} active={active} item="Our Courses">
-        <div className="flex flex-col space-y-4 text-sm">
-        <HoveredLink href="/courses">
-          All courses
-        </HoveredLink>
-        </div>
-        </MenuItem>
-        </Link>
+    <div
+    className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+    >
+        <Menu setActive={setActive}>
+            <Link href={"/"}>
+            <MenuItem setActive={setActive} active={active} item="Home">
+            
+            </MenuItem>
+            </Link>
+            <MenuItem
+            setActive={setActive} active={active} item="Our Courses"
+            >
+               <div className="flex flex-col space-y-4 text-sm">
+               <HoveredLink href="/courses">All Courses</HoveredLink>
+               <HoveredLink href="/courses">Besic Photography Theory</HoveredLink>
+               <HoveredLink href="/courses">Advance Compositions</HoveredLink>
+               <HoveredLink href="/courses">Photography Products</HoveredLink>
+          
     </div>
-  );
+    </MenuItem>
+    <Link href={"/contact"}>
+    <MenuItem setActive={setActive} active={active} item="contact Us">
+    </MenuItem>
+    </Link>
+    </Menu>
+    </div>
+  )
 }
 
 export default Navbar
