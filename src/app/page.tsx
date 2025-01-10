e7665c9f5c2a06f59e8a604145da97b69935c81f
soham408 <bambadesoham408@gmail.com>
2025-01-10 16:38:09 +0530

    featured Card section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { main } from "framer-motion/client";
 import Image from "next/image";
 import HeroSection from "@/components/ui/HeroSection";
+import FeaturedCourses from "@/components/FeaturedCourses";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[096] antialisaed bg-grid-white/[0.02]">
     <HeroSection />
+    <FeaturedCourses />
     </main>
   );
 }
